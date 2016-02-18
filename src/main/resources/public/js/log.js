740420484f63db23a6f7d234f886e2f9abc8a050
YichaoChen <cyc@CYCdeMacBook-Pro.local>
2016-02-17 22:58:43 -0500

    Presentation Tier

---
+++ src/main/resources/public/js/log.js
@@ -0,0 +1,26 @@
+function InputCheck(LoginForm)
+{
+  if (LoginForm.username.value == "")
+  {
+    alert("Please enter your username!");
+    LoginForm.username.focus();
+	return (false);
+  }else if (LoginForm.password.value == "")
+  {
+    alert("Please enter your password!");
+    LoginForm.password.focus();
+    return (false);
+  }else if (LoginForm.password.value !== LoginForm.confirmpassword.value)
+  {
+    alert("Password does not match!");
+    LoginForm.password.focus();
+    return (false);
+  }else if (LoginForm.email.value == "")
+  {
+    alert("Please enter your email address!");
+    LoginForm.password.focus();
+    return (false);
+  }else{
+	return (true); 
+  }
+}
